eab17cbe340a9f858e4672fca4ff3812c7cc0e40
Scott Cooper <scttcper@gmail.com>
2018-06-07 14:55:03 -0700

    do now

--- src/cron.ts
+++ src/cron.ts
@@ -15,3 +15,11 @@ setInterval(
       }),
   14400 * 1000, // 4 hours
 );
+
+
+build()
+    .then(() => debug(chalk.greenBright('build success!')))
+    .catch(e => {
+      console.error(e);
+      process.exit(1);
+    });
