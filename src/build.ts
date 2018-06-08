4d97aa50b161f4afbfe90f87803ca46ea50dff0c
Scott Cooper <scttcper@gmail.com>
2018-06-07 23:00:33 -0700

    Revert "remove async generator"
    This reverts commit 09679500b07a8abe60c84b453dee5579b27c594a.

--- src/build.ts
+++ src/build.ts
@@ -26,6 +26,15 @@ function difference(object: any, base: any): any {
   });
 }
 
+async function* allAlgolia() {
+  let browser = await index.browse('');
+  yield browser.hits as any[];
+  while (browser.cursor) {
+    browser = await index.browseFrom(browser.cursor);
+    yield browser.hits as any[];
+  }
+}
+
 async function updatePackage(pkg: any) {
   let info: any = await npm.get(pkg.name);
   info.objectID = pkg.name;
@@ -36,15 +45,9 @@ async function updatePackage(pkg: any) {
 
 export async function build() {
   // update new packages
-  let browser: any = false;
-  while (!browser || browser.cursor) {
-    if (browser) {
-      browser = await index.browseFrom(browser.cursor);
-    } else {
-      browser = await index.browse('');
-    }
+  for await (const packages of allAlgolia()) {
     const add: any[] = [];
-    for (const pkg of browser.hits) {
+    for (const pkg of packages) {
       // remove extraneous packages
       if (!allPackages.includes(pkg.name)) {
         debug(chalk.red('Removing'), pkg.name, pkg.objectID);
@@ -69,7 +72,6 @@ export async function build() {
       await index.addObjects(add);
     }
   }
-
   // add new packages
   for (const group of _.chunk<string>(allPackages, 100)) {
     const add: any[] = [];
